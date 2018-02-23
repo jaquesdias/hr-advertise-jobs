8ae5b99d07c7b2e95e5a84f9fd699d6b666b50da
Jaques Dias <jaquesdias19@gmail.com>
2018-02-23 17:31:35 -0300

    Call services on controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,7 +1,14 @@
 class CampaignsController < ApplicationController
   def index
+    upsert_campaign_instance.call
     campaigns = Campaign.all
 
     render json: campaigns
+  end
+
+  private
+
+  def upsert_campaign_instance
+    UpsertCampaignService.new(AdService.all_ads)
   end
 end
